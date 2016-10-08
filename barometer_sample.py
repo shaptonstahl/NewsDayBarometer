071d65621f836169152d19322303dab9a4799d1e
Stephen Haptonstahl <srh@haptonstahl.org>
2016-10-08 09:58:42 -0400

    does counts

--- barometer_sample.py
+++ barometer_sample.py
@@ -31,15 +31,16 @@ import unshorten_url
 import twitter_credentials
 
 WORKING_PATH = '/Users/stephenh/github/NewsDayBarometer/temp'
-DATA_PATH = '/Users/stephenh/github/NewsDayBarometer/data'
+DATA_PATH = '/Users/stephenh/github/NewsDayBarometer/counts'
 CATEGORY_INPUT_FOLDER = '/Users/stephenh/github/NewsDayBarometer/outlets/v_2016-10-03'
-SAMPLE_SIZE = 100
+SAMPLE_SIZE = 1000
 # can stream about 1000 / minute
 # unshortening takes...?
 
 temp_file_no_extension = WORKING_PATH + '/' + datetime.datetime.now().strftime('%F_%H-%M-%S')
 temp_stream_file = temp_file_no_extension + '.csv'
 temp_unshortened_file = temp_file_no_extension + '_unshortened.csv'
+output_filename = DATA_PATH + '/counts_' + datetime.datetime.now().strftime('%F') + '.csv'
 
 if not os.path.exists(WORKING_PATH):
 	os.makedirs(WORKING_PATH)
@@ -65,8 +66,8 @@ class listener(StreamListener):
 			return False
 
 #initialize twitter api
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+auth.set_access_token(twitter_credentials.ACCESS_TOKEN_KEY, twitter_credentials.ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
@@ -84,6 +85,8 @@ except:
 	print sys.exc_info()[0]
 	print 'error!'
 	stream.disconnect()
+
+print 'Sample of ' + str(SAMPLE_SIZE) + ' tweets collected'
 
 # unshorten the urls
 print 'Unshortening the urls...'
@@ -111,8 +114,34 @@ for i in range(len(file_names)):
 		lines = whole_file.split('\r')
 		for line in lines:
 			fields = line.split(',')
-			if fields[0] == 'Outlet':
+			if fields[0] == 'Outlet' or fields[0] == '':
 				continue
 			row = {'outlet': fields[0], 'url': fields[1], 'twitter': fields[2], 'short url': fields[3]}
 			dataset.append(row)
 	outlets[categories[i]] = dataset
+
+def check_url_against_category(url, category):
+	url_parts = [x['url'] for x in outlets[category]]
+	url_in_category = False
+	for url_part in url_parts:
+		url_part_re = url_part.replace('.', '\\.').replace('/', '\\/')
+		if re.search(url_part_re, url):
+			url_in_category = True
+	return url_in_category
+
+print 'Checking urls against list of media outlets...'
+counts = {}
+for category in categories:
+	counts[category] = 0
+
+with open(temp_unshortened_file, 'r') as f_unshortened:
+	for line in f_unshortened:
+		for category in categories:
+			if check_url_against_category(line, category):
+				counts[category] += 1
+
+with open(output_filename, 'w') as f_out:
+	for k, v in counts.iteritems():
+		f_out.write(k + ',' + str(v) + '\n')
+
+print 'Complete\n'
--- read_outlets.py
+++ read_outlets.py
@@ -1,27 +1,35 @@
 import os
 import re
 
-INPUT_FOLDER = '/Users/stephenh/github/NewsDayBarometer/outlets/v_2016-10-03'
+CATEGORY_INPUT_FOLDER = '/Users/stephenh/github/NewsDayBarometer/outlets/v_2016-10-03'
 
-raw_file_names = os.listdir(INPUT_FOLDER)
+raw_file_names = os.listdir(CATEGORY_INPUT_FOLDER)
 file_names = [x for x in raw_file_names if re.search('\.csv$', x)]
 
 categories = [x.replace('.csv', '') for x in file_names]
 
-# data structure: dictionary with key=file name stem and value = array of dictionaries
+# data structure: dictionary with key=category (stem of file name) and value = array of dictionaries
 # each dictionary is a row in the dataset
-
 outlets = {}
 
 for i in range(len(file_names)):
 	dataset = []
-	with open(os.path.join(INPUT_FOLDER, file_names[i]), 'r') as f:
+	with open(os.path.join(CATEGORY_INPUT_FOLDER, file_names[i]), 'r') as f:
 		whole_file = f.readline()
 		lines = whole_file.split('\r')
 		for line in lines:
 			fields = line.split(',')
-			if fields[0] == 'Outlet':
+			if fields[0] == 'Outlet' or fields[0] == '':
 				continue
 			row = {'outlet': fields[0], 'url': fields[1], 'twitter': fields[2], 'short url': fields[3]}
 			dataset.append(row)
 	outlets[categories[i]] = dataset
+
+def check_url_against_category(url, category):
+	url_parts = [x['url'] for x in outlets[category]]
+	url_in_category = False
+	for url_part in url_parts:
+		url_part_re = url_part.replace('.', '\\.').replace('/', '\\/')
+		if re.search(url_part_re, url):
+			url_in_category = True
+	return url_in_category
