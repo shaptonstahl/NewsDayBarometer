02eb26c7b8dc7ee0ee26166196dbce5fcc0647f6
Stephen Haptonstahl <srh@haptonstahl.org>
2025-05-18 16:28:31 -0400

    Update url unshortening

--- unshorten_url.py
+++ unshorten_url.py
@@ -2,54 +2,22 @@
 resolve url shorteners to where they eventually point
 """
 
-import re
-import httplib
+import requests
 
 
-def split_url_to_host_and_path(url):
+def unshorten_url(short_url, timeout=10):
+    """
+    Resolves a shortened URL to its final destination.
+
+    Parameters:
+    - short_url (str): The shortened URL.
+    - timeout (int): Timeout in seconds for the request.
+
+    Returns:
+    - str: The final destination URL after all redirects.
+    """
     try:
-        url = re.search("https{0,1}:\/\/(.*)", url).group(1)
-        if len(re.findall("\/", url)) == 0:
-            url = url + "/"
-        host = re.match("([^\/]*)\/.*", url).group(1)
-        path = re.search("[^\/]*(\/.*$)", url).group(1)
-    except:
-        return None
-    return host, path
-
-
-def probe_url(url):
-    host, path = split_url_to_host_and_path(url)
-    conn = httplib.HTTPConnection(host)
-    conn.request("HEAD", path)
-    res = conn.getresponse()
-    conn.close()
-    return res
-
-
-def unshorten_url(url, max_redirects=3):
-    n_redirects = 0
-    try:
-        res = probe_url(url)
-        while re.match("3", str(res.status)):
-            n_redirects += 1
-            try:
-                redirected_url = [x[1] for x in res.getheaders() if x[0] == "location"][
-                    0
-                ]
-                if re.match("\/", redirected_url):
-                    url = (
-                        re.match("(https{0,1}:\/\/)", url).group(1)
-                        + split_url_to_host_and_path(url)[0]
-                        + redirected_url
-                    )
-                else:
-                    url = redirected_url
-                res = probe_url(url)
-            except:
-                break
-            if n_redirects >= max_redirects:
-                break
-        return url.strip()
-    except:
-        return None
+        response = requests.head(short_url, allow_redirects=True, timeout=timeout)
+        return response.url
+    except requests.RequestException as e:
+        return f"Error: {e}"
